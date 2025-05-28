35190a2209d6f1c69e1ff1fcab150f4f178c9437
AmitojhS <104732409+AmitojhS@users.noreply.github.com>
2025-05-27 22:10:42 -0400

    Added Lucky Card

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -22,7 +22,9 @@ public class CardTrick {
     {
         
         Card[] magicHand = new Card[7]; //Array of object
-        Card luckyCard = new Card(4,"diamonds"); //Added object from github
+        Card luckyCard = new Card(); //Added object from github
+        luckyCard.setValue(2);
+        luckyCard.setSuits("Clubs");
 
         
         for( int i=0;i<magicHand.length;i++)
